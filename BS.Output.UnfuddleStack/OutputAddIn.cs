7861c41142ca4764c334bda25bc8f1fdf2a1c831
Alexej Hirsch <alexej@bugshooting.com>
2017-09-16 11:54:18 +0200

    Projektdateien hinzufügen.

---
+++ BS.Output.UnfuddleStack/OutputAddIn.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace BS.Output.UnfuddleStack
+{
+  public class OutputAddIn: V3.OutputAddIn<Output>
+  {
+
+    protected override string Name
+    {
+      get { return "Unfuddle STACK"; }
+    }
+
+    protected override Image Image64
+    {
+      get  { return Properties.Resources.logo_64; }
+    }
+
+    protected override Image Image16
+    {
+      get { return Properties.Resources.logo_16 ; }
+    }
+
+    protected override bool Editable
+    {
+      get { return true; }
+    }
+
+    protected override string Description
+    {
+      get { return "Attach screenshots to Unfuddle STACK messages or tickets."; }
+    }
+    
+    protected override Output CreateOutput(IWin32Window Owner)
+    {
+      
+      Output output = new Output(Name, 
+                                 String.Empty, 
+                                 String.Empty, 
+                                 String.Empty, 
+                                 "Screenshot",
+                                 String.Empty, 
+                                 true,
+                                 String.Empty,
+                                 "1");
+
+      return EditOutput(Owner, output);
+
+    }
+
+    protected override Output EditOutput(IWin32Window Owner, Output Output)
+    {
+
+      Edit edit = new Edit(Output);
+
+      var ownerHelper = new System.Windows.Interop.WindowInteropHelper(edit);
+      ownerHelper.Owner = Owner.Handle;
+      
+      if (edit.ShowDialog() == true) {
+
+        return new Output(edit.OutputName,
+                          edit.Url,
+                          edit.UserName,
+                          edit.Password,
+                          edit.FileName,
+                          edit.FileFormat,
+                          edit.OpenItemInBrowser,
+                          Output.LastProjectID,
+                          Output.LastIssueID);
+      }
+      else
+      {
+        return null; 
+      }
+
+    }
+
+    protected override OutputValueCollection SerializeOutput(Output Output)
+    {
+
+      OutputValueCollection outputValues = new OutputValueCollection();
+
+      outputValues.Add(new OutputValue("Name", Output.Name));
+      outputValues.Add(new OutputValue("Url", Output.Url));
+      outputValues.Add(new OutputValue("UserName", Output.UserName));
+      outputValues.Add(new OutputValue("Password",Output.Password, true));
+      outputValues.Add(new OutputValue("OpenItemInBrowser", Convert.ToString(Output.OpenItemInBrowser)));
+      outputValues.Add(new OutputValue("FileName", Output.FileName));
+      outputValues.Add(new OutputValue("FileFormat", Output.FileFormat));
+      outputValues.Add(new OutputValue("LastProjectID", Output.LastProjectID));
+      outputValues.Add(new OutputValue("LastIssueID", Output.LastIssueID));
+
+      return outputValues;
+      
+    }
+
+    protected override Output DeserializeOutput(OutputValueCollection OutputValues)
+    {
+
+      return new Output(OutputValues["Name", this.Name].Value,
+                        OutputValues["Url", ""].Value, 
+                        OutputValues["UserName", ""].Value,
+                        OutputValues["Password", ""].Value, 
+                        OutputValues["FileName", "Screenshot"].Value, 
+                        OutputValues["FileFormat", ""].Value,
+                        Convert.ToBoolean(OutputValues["OpenItemInBrowser", Convert.ToString(true)].Value),
+                        OutputValues["LastProjectID", string.Empty].Value, 
+                        OutputValues["LastIssueID", "1"].Value);
+
+    }
+
+    protected override async Task<V3.SendResult> Send(IWin32Window Owner, Output Output, V3.ImageData ImageData)
+    {
+
+      try
+      {
+
+        // TODO
+        return null;
+
+      }
+      catch (Exception ex)
+      {
+        return new V3.SendResult(V3.Result.Failed, ex.Message);
+      }
+
+    }
+      
+  }
+}
---
+++ BS.Output.UnfuddleStack/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BS.Output.UnfuddleStack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bug Shooting")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("90083601-6D87-440D-832C-43A92E812D4B")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
